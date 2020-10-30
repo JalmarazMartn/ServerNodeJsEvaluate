1ef6f6146e5f4e52afc8e07614ecc6aa27098d4f
Jesús Almaraz Martín <44129743+JalmarazMartn@users.noreply.github.com>
2020-10-30 05:42:38 +0100

    Add files via upload

--- ServidorHttp.js
+++ ServidorHttp.js
@@ -3,43 +3,41 @@ const http = require('http');
 const { strict } = require('assert');
 const { on } = require('process');
 //const url = require('url');
-const server = http.createServer((request, response) => {       
-       let boolexpr = '';
-       request.on('data', function(data) {
-              for (var i = 0; i < data.length; i++) {
-                     boolexpr += String.fromCharCode(data[i]);
-                   }                                 
-              response.writeHead(200, {'Content-Type': 'text/plain'});   
-              //console.log(AlmostSafe(SQLToJSBooleanNotation(boolexpr)));
-              //console.log(IsTrue(boolexpr));              
-              console.log('Input ' + boolexpr + '=' + IsTrue(boolexpr));
-              response.end(boolexpr+ ' ' + AlmostSafe(boolexpr) + '==>' + IsTrue(boolexpr))});       
-            })       
+const server = http.createServer((request, response) => {
+  let boolexpr = '';
+  request.on('data', function (data) {
+    for (var i = 0; i < data.length; i++) {
+      boolexpr += String.fromCharCode(data[i]);
+    }
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    //console.log(AlmostSafe(SQLToJSBooleanNotation(boolexpr)));
+    //console.log(IsTrue(boolexpr));              
+    console.log('Input ' + boolexpr + '=' + IsTrue(boolexpr));
+    response.end(boolexpr + ' ' + AlmostSafe(boolexpr) + '==>' + IsTrue(boolexpr))
+  });
+})
 server.listen(8080);
 console.log('Service up');
-function IsTrue(inputexpr='textdef'){
-try{
-var inputexprModified = SQLToJSBooleanNotation(inputexpr);
-inputexprModified = AlmostSafe(inputexprModified);
-if (eval(AlmostSafe(inputexprModified))) {
-return('true')       
-} else {
-return('false')
+function IsTrue(inputexpr = 'textdef') {
+  try {
+    var inputexprModified = SQLToJSBooleanNotation(inputexpr);
+    inputexprModified = AlmostSafe(inputexprModified);
+    if (eval(AlmostSafe(inputexprModified))) {
+      return ('true')
+    } else {
+      return ('false')
+    }
+  }
+  catch (error) { return (error) };
 }
+function AlmostSafe(original) {
+  return (original.replace(/[A-Za-z]/g, ''));
 }
-catch(error)
-{return(error)};
-}
-function AlmostSafe(original)
-{
-    return(original.replace(/[A-Za-z]/g,''));
-}
-function SQLToJSBooleanNotation(original)
-{
-    var JSNotation = original;
-    JSNotation = JSNotation.replace(/and/gi,'&&');
-    JSNotation = JSNotation.replace(/or/gi,'||');
-    JSNotation = JSNotation.replace(/not/gi,'!');
-    JSNotation = JSNotation.replace(/([^=])(=)([^=])/g,"$1 == $3");
-    return(JSNotation);
+function SQLToJSBooleanNotation(original) {
+  var JSNotation = original;
+  JSNotation = JSNotation.replace(/and/gi, '&&');
+  JSNotation = JSNotation.replace(/or/gi, '||');
+  JSNotation = JSNotation.replace(/not/gi, '!');
+  JSNotation = JSNotation.replace(/([^=&^>&^<])(=)([^=])/g, "$1 == $3");
+  return (JSNotation);
 }        
